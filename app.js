365dee21b1e851f55007695eb709272445553bef
proabre <proabresh@gmail.com>
2026-09-17 14:02:37 +0300

    routing and html in express

--- app.js
+++ app.js
@@ -17,5 +17,19 @@ app.listen(port, () => {
 // Handle GET requests to the home page "/"
 app.get("/", (req, res) => {
   // Send a response back to the browser
-  res.send("<p>Express home page</p>");
+  // res.send("<p>Express home page</p>");
+  res.sendFile("./views/index.html", { root: __dirname });
 });
+
+app.get("/about", (req, res) => {
+  // Send a response back to the browser
+  //res.send("<p>Express home page</p>");
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact", (req, res) => {
+  // Send a response back to the browser
+  //res.send("<p>Express home page</p>");
+  res.sendFile("./views/contact-me.html", { root: __dirname });
+});
+//Express.js is a web framework for Node.js. It helps you build web servers and APIs more easily than using Node's built-in http module directly.
